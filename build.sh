970a87b961179beef7c31e55ec6d6cb852822066
Godu <i@ysicing.me>
2019-09-15 23:56:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
+export PATH=$PATH:~/bin
 #=================================================
 #       System Required: CentOS/Debian/Ubuntu/Darwin
 #       Description: AUXPI build
